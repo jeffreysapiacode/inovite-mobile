e96c7118b0c1357f71b98a30e2a781ac44c4ac23
jeffreysapia <jeffsapia1988@gmail.com>
2026-05-28 14:05:53 -0400

    Reset point

--- lib/core/widgets/colored_square.dart
+++ lib/core/widgets/colored_square.dart
@@ -16,28 +16,27 @@ class ColoredSquare extends StatelessWidget {
     required this.height,
     required this.color,
     required this.borderRadius
-  })
+  });
 
   @override
   Widget build(BuildContext context) {
     return Transform.rotate(
-          angle: rotate * (math.pi / 180),
-          child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(borderRadius),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color(0xFF000000),
-                  offset: Offset(4, 4),
-                  blurRadius: 0,
-                ),
-              ],
+      angle: rotate * (math.pi / 180),
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(borderRadius),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0xFF000000),
+              offset: Offset(4, 4),
+              blurRadius: 0,
             ),
-          ),
-        );
+          ],
+        ),
+      ),
+    );
   }
-
 }
