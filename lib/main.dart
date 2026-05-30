8c9497346e03e703abaff66c4782113b56adac06
jeffreysapia <jeffsapia1988@gmail.com>
2026-05-30 11:31:08 -0400

    Setting baseline for reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:inovite_mobile/features/screens/auth/login_screen.dart';
-import 'package:inovite_mobile/features/screens/auth/signup_screen.dart';
-import 'package:inovite_mobile/features/screens/auth/welcome_screen.dart';
+import '../../../../screen/auth/welcome_screen.dart';
 
 void main() {
   runApp(Inovite());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:inovite_mobile/features/screens/auth/welcome_screen.dart';
 
-import 'package:inovite_mobile/main.dart';
+import 'package:inovite_mobile/screen/auth/welcome_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
